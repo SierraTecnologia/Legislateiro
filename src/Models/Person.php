5c6271464ca55bb06220736ad7146145bd8d274c
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-09-13 19:25:26 -0300

    fix laravel8 compatibility

--- src/Models/Person.php
+++ src/Models/Person.php
@@ -4,9 +4,8 @@ namespace Legislateiro\Models\Actors;
 
 // use Legislateiro\Traits\AsHuman;
 // use Muleta\Utils\Modificators\StringModificator;
-// use Spatie\MediaLibrary\HasMedia\HasMedia;
 use Telefonica\Models\Actors\Person as Base;
 
-class Person extends Base // implements HasMedia
+class Person extends Base
 {
 }
