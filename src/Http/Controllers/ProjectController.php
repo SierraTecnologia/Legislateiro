bfbf3a27811e1d733c5687809fa3a1528f516cb4
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2021-04-02 16:43:23 -0300

    CE-11

--- src/Http/Controllers/ProjectController.php
+++ src/Http/Controllers/ProjectController.php
@@ -36,7 +36,7 @@ class ProjectController extends Base
         // 'to',
         // 'code' => [
         //     'type' => 'select',
-        //     'options' => 'Facilitador\Models\RedirectRule::getCodes()',
+        //     'options' => 'Pedreiro\Models\RedirectRule::getCodes()',
         // ],
         // 'label',
     ];
@@ -73,7 +73,7 @@ class ProjectController extends Base
         //     'code' => [
         //         'label' => __('pedreiro::redirect_rules.controller.search.code'),
         //         'type' => 'select',
-        //         'options' => 'Facilitador\Models\RedirectRule::getCodes()',
+        //         'options' => 'Pedreiro\Models\RedirectRule::getCodes()',
         //     ],
         //     'label' => [
         //         'label' => __('pedreiro::redirect_rules.controller.search.label'),
